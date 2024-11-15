53af23e924e7aa90155bdecb6f78f6a0ed22bc63
Sarah Moorby <lhijd8vq@students.codeinstitute.net>
2024-11-15 13:38:24 +0000

    dont want to break it

--- cathat/catarticles/views.py
+++ cathat/catarticles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from.models import Cat
-
+from django.http import HttpResponse
 def article_list(request):
     dog = Cat.objects.all().order_by('date')
     return render(request, 'catarticles/article_list.html', {'dog': dog})
+
+def article_details(request, slug):
+    dog = Cat.objects.get(slug=slug)
+    return render(request, 'catarticles/article_detail.html', {'article': dog})
